8a19d024844977e9ab36de51afdc9e99c8ce134e
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2020-04-20 15:27:24 -0400

    made function to show all addresses

--- routes/index.js
+++ routes/index.js
@@ -8,22 +8,31 @@ const Address = require('./models/Address')
 
 
 /* GET home page. */
-router.get('/location', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.json({message:'hello world'});
 });
-
+//get single longitude and latitude
 router.get('/location',(req,res)=>{
   const address = req.body.address 
   const url = `https://api.opencagedata.com/geocode/v1/json?q=${address}&key=${key}`
   axios.get(url)
   .then(response=>{return response})
   .then((response)=>{
-   
+    
     return res.status(200).send(response.data.results[0].annotations.DMS)
 
   })
 })
 
+//get all addresses
+router.get('/location',(req,res)=>{
+  Address.find({}).then((address)=>{
+    return res.send(address)
+  })
+})
+
+
+//save an address 
 router.post('/location',(req,res)=>{
   Address.findById({id:_id}).then((address)=>{
     if(address) return res.status(418).json({message:'address is already on file'})
@@ -37,5 +46,7 @@ router.post('/location',(req,res)=>{
     return res.status(418).json({error:'address not saved'})
   })
 })
+
+
 
 module.exports = router;
