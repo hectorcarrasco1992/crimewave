1e69922ba7632f751ce691a608b043975d203929
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2020-04-20 15:12:05 -0400

    added post route to route that gets long and latitude to send savd address to the database

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,38 @@ const axios = require('axios')
 const NodeGeocoder = require('node-geocoder')
 const key = process.env.KEY
 
-const address = '359 south 2nd street new york 11211'
-const url = `https://api.opencagedata.com/geocode/v1/json?q=${address}&key=${key}`
-
+const Address = require('./models/Address')
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/location', function(req, res, next) {
   res.json({message:'hello world'});
 });
 
 router.get('/location',(req,res)=>{
-  axios.get(url).then(response=>console.log(response.data.results[0].annotations.DMS))
+  const address = req.body.address 
+  const url = `https://api.opencagedata.com/geocode/v1/json?q=${address}&key=${key}`
+  axios.get(url)
+  .then(response=>{return response})
+  .then((response)=>{
+   
+    return res.status(200).send(response.data.results[0].annotations.DMS)
+
+  })
+})
+
+router.post('/location',(req,res)=>{
+  Address.findById({id:_id}).then((address)=>{
+    if(address) return res.status(418).json({message:'address is already on file'})
+    const newAddress = new Address
+    newAddress.address = req.body.address
+    newAddress.save()
+
+    return res.status(200).json({message:'address saved'})
+  }).catch(err=>{
+    console.log(err)
+    return res.status(418).json({error:'address not saved'})
+  })
 })
 
 module.exports = router;
