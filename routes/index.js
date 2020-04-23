8805b22ca0fd784a4c5e8474e7fdc1adaff0e8cb
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2020-04-23 12:20:26 -0400

    able to post a new crime

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios')
 let crimeData = require('./CrimeData.json')
-console.log(crimeData)
+
 const key = process.env.KEY
 const date = require('../utils/Date')
 
@@ -15,65 +15,14 @@ router.get('/', function(req, res, next) {
 });
 //get single longitude and latitude
 router.post('/crime',(req,res)=>{
-    let newCrime = new Crimes
-    newCrime.crime = req.body.crime
-    newCrime.date = req.body.date
+  let newCrime = new Crimes
+  newCrime.crime = req.body.crime
+  newCrime.date = req.body.date
+  newCrime.save()
+  return res.send(newCrime)
     })
 
-  //const url = `https://api.opencagedata.com/geocode/v1/json?q=${address}&key=${key}`
-  // axios.get(url)
-  // .then((response)=>{return response})
-  // .then((response)=>{
-  //   let lat = response.data.results[0].geometry.lat
-  //   let lng = response.data.results[0].geometry.lng 
-  //   let coordinates = []
-  //   coordinates.push(lat)
-  //   coordinates.push(lng)
-  //   console.log(coordinates)
-  //   return coordinates
-  //   // return res.status(200).send(response.data.results[0].annotations.DMS)
 
-  // }).then((coordinates)=>{
-  //   const crimeUrl = `https://api.crimeometer.com/v1/incidents/raw-data?lat=${coordinates[0]}&lon=${coordinates[1]}&distance=10mi&datetime_ini=${date()}&datetime_end=${new Date().toISOString()}&page=${1}`
-  //   // const crimeUrl = `https://api.crimeometer.com/v1/incidents/raw-data?lat=36.47016&lon=10.47744&distance=10mi&datetime_ini=${date()}&datetime_end=${new Date().toISOString()}&page=${1}`
-
-  //   axios.get(crimeUrl,{
-  //     headers:{
-  //       'Content-Type':'application/json',
-  //       'x-api-key':'k3RAzKN1Ag14xTPlculT39RZb38LGgsG8n27ZycG'
-  //     }
-  //   }).then((crime)=>{
-  //     console.log(crime.data)
-  //     // const newCrime = new Crimes
-  //     // newCrime.crime = crime.data.incidents[0].incident_offense
-      
-  //     // newCrime.save()
-  //     // return res.send(newCrime)
-  //   }).catch(err=>console.log(err))
-          
-      
-  //   //  }).catch(err=>console.log(err))
-    
-  // }).catch(err=>console.log(err))
-})
-
-
-
-
-// router.post('/location',(req,res)=>{
-//   Crimes.findById({id:_id}).then((crime)=>{
-//     if(crime) return res.status(418).json({message:'address is already on file'})
-//     const newCrime = new Crime
-//     newCrime.crime = req.body.crime
-//     newCrime.description = req.body.description
-//     newAddress.save()
-
-//     return res.status(200).json({message:'address saved'})
-//   }).catch(err=>{
-//     console.log(err)
-//     return res.status(418).json({error:'address not saved'})
-//   })
-// })
 
 
 
